2966fde15f9e7c5612851d490e16de66410fc780
Yoelvis  Jiménez <43357527+yoelvisj@users.noreply.github.com>
2019-05-18 22:49:34 -0400

    Create one-dim-cellular-automaton.py

---
+++ one-dim-cellular-automaton.py
@@ -0,0 +1,17 @@
+"""
+Consider the one-dimentional cellular automaton, consisting of a array of cells.
+Each cell contains an integer, 1 or 0.
+A cell containing a 1 means occupied, and a 0 means unoccupied. 
+On each time step, we update each cell according to the following table of rules,
+one for each combination of a current cell and it left and right neighbors.
+Left neighbor | Current cell | Right neighbor | New cell
+      1       |      1       |       1        |    0
+      1       |      1       |       0        |    1
+      1       |      0       |       1        |    1
+      1       |      0       |       0        |    0
+      0       |      1       |       1        |    1
+      0       |      1       |       0        |    1
+      0       |      0       |       1        |    1
+      0       |      0       |       0        |    0
+The cells at each end of the array have only one neighbor each and are not updated.
+"""
