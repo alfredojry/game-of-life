a9de68cd4c5b41090d107ab3b4d9d24678a1e59f
Yoelvis  Jiménez <43357527+yoelvisj@users.noreply.github.com>
2019-05-19 00:47:53 -0400

    Update one-dim-cellular-automaton.py

--- one-dim-cellular-automaton.py
+++ one-dim-cellular-automaton.py
@@ -17,3 +17,65 @@ The cells at each end of the array have only one neighbor each and are not updat
 A good website on cellular automata may be found in Wolfram's website:
 http://mathworld.wolfram.com/CellularAutomaton.html
 """
+"""
+Example #1:
+$ python3 ca.py
+Welcome to the cellular automaton simulation!
+Enter number of cells, C: (<= 80): 10
+Enter number of time steps, N: 10
+Enter the indices of occupied cells (space-separated): 4 6
+"""
+"""
+Example #2:
+$ python3 ca.py
+Welcome to the cellular automaton simulation!
+Enter number of cells, C: (<= 80): 40
+Enter number of time steps, N: 30
+Enter the indices of occupied cells (space-separated): 38
+"""
+
+# Cellular Automata
+print('Welcome to the cellular automaton simulation!')
+
+# Prompt the user to enter the number of cells, C
+C = input('Enter number of cells, C: (<= 80): ')
+# Prompt the user to enter time steps, N
+N = input('Enter number of time steps, N: ')
+# Prompt the user to enter the list index of cells that initially contain 1 (space-separed)
+occupieds = input('Enter the indices of occupied cells (space-separated): ')
+
+def updateCells(cells):
+    newList = []
+    for i in range(len(cells)):
+        if i == 0 or i == len(cells) - 1:
+            newList.append(cells[i])
+        else:
+            left, right = cells[i - 1], cells[i + 1]
+            current = cells[i]
+            if left == 1 and current == 1 and right == 1:
+                newList.append(0)
+            elif left == 1 and current == 0 and right == 0:
+                newList.append(0)
+            elif left == 0 and current == 0 and right == 0:
+                newList.append(0)
+            else:
+                newList.append(1)
+    return newList
+
+def displayCells(cells):
+    result = ''
+    for cell in cells:
+        result += '#' if cell else ' '
+    print(result)
+
+cells = [0 for _ in range(int(C))]
+indices = [int(char) for char in occupieds.split(' ')]
+for index in indices:
+    cells[index] = 1
+
+lastDigits = ''.join(str(n)[-1] for n in range(int(C)))
+print(lastDigits)
+displayCells(cells)
+for i in range(int(N)):
+    cells = updateCells(cells)
+    displayCells(cells)
